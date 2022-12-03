567c83f837ac485da0a98e09d4b01965cdb6fbae
Ziyang Li <paseaf@me.com>
2022-12-03 17:57:52 +0100

    Day1 part2 done

--- day1/main.go
+++ day1/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"container/heap"
 	"fmt"
 	"log"
 	"os"
@@ -10,8 +11,11 @@ import (
 )
 
 func main() {
-	result := findMaxCalories(filepath.Join("day1", "input.txt"))
-	fmt.Printf("Result is %v\n", result)
+	result1 := findMaxCalories(filepath.Join("day1", "input.txt"))
+	fmt.Printf("Result1 is %v\n", result1)
+
+	result2 := findTopThree(filepath.Join("day1", "input.txt"))
+	fmt.Printf("Result2 is %v\n", result2)
 }
 
 func findMaxCalories(path string) (result int) {
@@ -36,6 +40,34 @@ func findMaxCalories(path string) (result int) {
 	return result
 }
 
+func findTopThree(path string) (result int) {
+	// 1. read input
+	file, err := os.Open(path)
+	check(err)
+	defer file.Close()
+
+	maxHeap := &IntHeap{}
+	heap.Init(maxHeap)
+	scanner := bufio.NewScanner(file)
+	curResult := 0
+	for scanner.Scan() {
+		line := scanner.Text()
+		if len(line) == 0 {
+			heap.Push(maxHeap, curResult)
+			curResult = 0
+		} else {
+			curNum, err := strconv.Atoi(line)
+			check(err)
+			curResult += curNum
+		}
+	}
+	// get top 3
+	for i := 0; i < 3; i++ {
+		result += heap.Pop(maxHeap).(int)
+	}
+	return result
+}
+
 func check(e error) {
 	if e != nil {
 		log.Fatal(e)
@@ -47,4 +79,26 @@ func max(a int, b int) int {
 		return a
 	}
 	return b
+}
+
+// An IntHeap is a MAX-heap of ints.
+// Modified from https://pkg.go.dev/container/heap#example-package-IntHeap
+type IntHeap []int
+
+func (h IntHeap) Len() int           { return len(h) }
+func (h IntHeap) Less(i, j int) bool { return h[i] > h[j] }
+func (h IntHeap) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
+
+func (h *IntHeap) Push(x any) {
+	// Push and Pop use pointer receivers because they modify the slice's length,
+	// not just its contents.
+	*h = append(*h, x.(int))
+}
+
+func (h *IntHeap) Pop() any {
+	old := *h
+	n := len(old)
+	x := old[n-1]
+	*h = old[0 : n-1]
+	return x
 }
--- day1/main_test.go
+++ day1/main_test.go
@@ -9,3 +9,10 @@ func Test_findMaxCalories(t *testing.T) {
 		t.Errorf("incorrect result: expected %v, got %v", expected, actual)
 	}
 }
+func Test_findTopThree(t *testing.T) {
+	expected := 45000
+	actual := findTopThree("./test_input.txt")
+	if expected != actual {
+		t.Errorf("incorrect result: expected %v, got %v", expected, actual)
+	}
+}
