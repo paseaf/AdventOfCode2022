7568a59d512cb02f11979b63cbf8704d2c7c7af7
Ziyang Li <paseaf@me.com>
2022-12-04 05:36:44 +0100

    Day2 part2 done

--- day2/main.go
+++ day2/main.go
@@ -9,8 +9,11 @@ import (
 )
 
 func main() {
-	score := calculateScore(filepath.Join("./day2", "input.txt"))
-	fmt.Printf("Total score: %v", score)
+	score1 := calculateScore(filepath.Join("./day2", "input.txt"))
+	fmt.Printf("Total score1: %v\n", score1)
+
+	score2 := calculateScore2(filepath.Join("./day2", "input.txt"))
+	fmt.Printf("Total score2: %v\n", score2)
 }
 
 func calculateScore(path string) (score int) {
@@ -40,4 +43,36 @@ var winScore = map[string]int{
 	"B Z": 3 + 6,
 	"C Z": 3 + 3,
 	"A Z": 3,
+}
+
+func calculateScore2(path string) (score int) {
+	file, err := os.Open(path)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		if len(line) > 0 {
+			score += winScorePart2[line]
+		}
+	}
+	return score
+}
+
+var winScorePart2 = map[string]int{
+	// lose
+	"A X": 3 + 0,
+	"B X": 1 + 0,
+	"C X": 2 + 0,
+	// draw
+	"A Y": 1 + 3,
+	"B Y": 2 + 3,
+	"C Y": 3 + 3,
+	// win
+	"B Z": 3 + 6,
+	"C Z": 1 + 6,
+	"A Z": 2 + 6,
 }
--- day2/main_test.go
+++ day2/main_test.go
@@ -9,3 +9,10 @@ func Test_calculateScore(t *testing.T) {
 		t.Errorf("incorrect result: expected %v, got %v", expected, actual)
 	}
 }
+func Test_calculateScore2(t *testing.T) {
+	expected := 12
+	actual := calculateScore2("./test_input.txt")
+	if expected != actual {
+		t.Errorf("incorrect result: expected %v, got %v", expected, actual)
+	}
+}
